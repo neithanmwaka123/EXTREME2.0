c1d62fe8e14a121f51e3a6b0cca288c39ffba9a7
nathan <143506651+neithanmwaka123@users.noreply.github.com>
2023-11-14 08:32:28 +0300

    nate

---
+++ java.js
@@ -0,0 +1,10 @@
+// hello world demo
+//let fname = "george";
+//let age = 19;
+//let individual = {
+    //fname : 'george',
+  //  age: 19
+//}
+//console.log(individual)
+let selectedcars = ['benz', 'vitz']
+console.log(selectedcars[0])
